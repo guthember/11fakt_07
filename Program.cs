e13013dae66e2079bc7e59256cc40877c1d8681a
Erik Gúth <guthember@gmail.com>
2019-10-03 14:54:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haromszog
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Három egész számot kérjünk be a felhasználótól
+      // Döntsük el szerkeszthető-e belőle háromszög?
+      // Hogyan dönthető el?
+      //     -- bármely két oldal összege nagyobb
+      //        kell legyen mint a harmadik         
+      // Kimenet: Szerkeszthető / Nem szerkeszthető
+
+      // Adatok bekérése
+      // Mindig helyes adatot adunk meg! (ha egész akkor egész)
+      int a, b, c;
+
+      Console.Write("Kérek egy egész számot: ");
+      a = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Kérek egy egész számot: ");
+      b = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Kérek egy egész számot: ");
+      c = Convert.ToInt32(Console.ReadLine());
+
+      // logikai változó 
+      // két értéke lehet: true, false
+      //bool igaz = true;
+      bool igaz;
+
+      // Eldöntjük, hogy szerkeszthető-e belőlük háromszög
+      //if (a + b < c) // ekkor nem
+      //{
+      //  igaz = false;
+      //}
+      //else if (a + c < b) // ekkor sem
+      //{
+      //  igaz = false;
+      //} 
+      //else if(b + c < a) // és ekkor sem
+      //{
+      //  igaz = false;
+      //}
+
+      // VAGY felhasználása
+      //if ( a + b < c || a + c < b || b + c < a )
+      //if ( (a + b < c) || (a + c < b) || (b + c < a) )
+      //{
+      //  igaz = false;
+      //}
+
+      if (a + b > c && a + c > b && b + c > a)
+      {
+        igaz = true;
+      }
+      else
+      {
+        igaz = false;
+      }
+
+      // ha "igaz" változó értéke 
+      // true -> akkor szerkeszthető
+      // false -> akkor nem szerkeszthető
+
+      // == összehasonlítás (egyenlőségre)
+      if ( igaz )
+      {
+        Console.WriteLine("Szerkeszthető");
+      }
+      else
+      {
+        Console.WriteLine("Nem szerkeszthető");
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ logika.md
@@ -0,0 +1,18 @@
+# Logika
+
+- és -> &&
+- vagy -> ||
+
+## VAGY
+A  | B | A vagy B
+I  | I |     I
+I  | H |     I
+H  | I |     I
+H  | H |     H
+
+## ÉS
+A  | B | A és B
+I  | I |     I
+I  | H |     H
+H  | I |     H
+H  | H |     H
